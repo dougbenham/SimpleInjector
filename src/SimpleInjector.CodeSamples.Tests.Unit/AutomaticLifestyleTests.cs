fb4b13a08235b003597b8e40bb7210caa85be1fe
dotnetjunkie <steven@cuttingedge.it>
2019-03-27 15:44:02 +0100

    Example. Doesn't work though.

---
+++ src/SimpleInjector.CodeSamples.Tests.Unit/AutomaticLifestyleTests.cs
@@ -0,0 +1,261 @@
+﻿namespace SimpleInjector.CodeSamples.Tests.Unit
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SimpleInjector.Lifestyles;
+
+    [TestClass]
+    public class AutomaticLifestyleTests
+    {
+        private static readonly Lifestyle Automatic = new AutomaticLifestyle();
+        private static readonly ScopedLifestyle Scoped = new AsyncScopedLifestyle();
+
+        [TestMethod]
+        public void Registration_TypeWithoutDependencies_Singleton()
+        {
+            // Arrange
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<NoDependencies2>();
+            container.Verify();
+
+            // Act
+            var reg = GetRegistration<NoDependencies2>(container);
+
+            // Assert
+            Assert.AreSame(Lifestyle.Singleton, reg.Lifestyle, $"Actual: {reg.Lifestyle.Name}");
+        }
+
+        [TestMethod]
+        public void Registration_TypeWithAllSingletonDependencies_Singleton()
+        {
+            // Arrange
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<DependingOn<NoDependencies1, NoDependencies2>>();
+
+            container.Register<NoDependencies1>(Lifestyle.Singleton);
+            container.Register<NoDependencies2>(Lifestyle.Singleton);
+
+            container.Verify();
+
+            // Act
+            var reg = GetRegistration<DependingOn<NoDependencies1, NoDependencies2>>(container);
+
+            // Assert
+            Assert.AreSame(Lifestyle.Singleton, reg.Lifestyle, $"Actual: {reg.Lifestyle.Name}");
+        }
+
+        [TestMethod]
+        public void Registration_TypeWithMixedLifestyles1_ShortestLifestyle()
+        {
+            // Arrange
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<DependingOn<NoDependencies1, NoDependencies2>>();
+
+            container.Register<NoDependencies1>(Lifestyle.Singleton);
+            container.Register<NoDependencies2>(Lifestyle.Scoped);
+
+            container.Verify();
+
+            // Act
+            var reg = GetRegistration<DependingOn<NoDependencies1, NoDependencies2>>(container);
+
+            // Assert
+            Assert.AreSame(Scoped, reg.Lifestyle, $"Actual: {reg.Lifestyle.Name}");
+        }
+
+        [TestMethod]
+        public void Registration_TypeWithMixedLifestyles2_ShortestLifestyle()
+        {
+            // Arrange
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<DependingOn<NoDependencies1, NoDependencies2>>();
+
+            container.Register<NoDependencies1>(Lifestyle.Scoped);
+            container.Register<NoDependencies2>(Lifestyle.Singleton);
+
+            container.Verify();
+
+            // Act
+            var registration = GetRegistration<DependingOn<NoDependencies1, NoDependencies2>>(container);
+
+            // Assert
+            Assert.AreSame(container.Options.DefaultScopedLifestyle, registration.Lifestyle);
+        }
+
+        [TestMethod]
+        public void Registration_TypeWithMixedLifestyles3_ShortestLifestyle()
+        {
+            // Arrange
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<DependingOn<NoDependencies1, NoDependencies2>>();
+
+            container.Register<NoDependencies1>(Lifestyle.Scoped);
+            container.Register<NoDependencies2>(Lifestyle.Transient);
+
+            container.Verify();
+
+            // Act
+            var registration = GetRegistration<DependingOn<NoDependencies1, NoDependencies2>>(container);
+
+            // Assert
+            Assert.AreSame(Lifestyle.Transient, registration.Lifestyle);
+        }
+
+        [TestMethod]
+        public void Registration_TypeWithMixedLifestyles4_ShortestLifestyle()
+        {
+            // Arrange
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<NoDependencies1>(Lifestyle.Transient);
+            container.Register<NoDependencies2>(Lifestyle.Singleton);
+
+            container.Register<DependingOn<NoDependencies1, NoDependencies2>>();
+            container.Verify();
+
+            // Act
+            var registration = GetRegistration<DependingOn<NoDependencies1, NoDependencies2>>(container);
+
+            // Assert
+            Assert.AreSame(Lifestyle.Transient, registration.Lifestyle);
+        }
+
+        [TestMethod]
+        public void Registration_DecoratorWithOnlySingletonDependencies_ProducesExpectedGraph()
+        {
+            // Arrange
+            string expectedGraph =
+@"DependingOn<IService>( // Singleton
+    ServiceDecoratorDependingOn<NoDependencies1>( // Singleton
+        DependingOn<NoDependencies1, NoDependencies2>( // Singleton
+            NoDependencies1(), // Singleton
+            NoDependencies2()), // Singleton
+        NoDependencies1())) // Singleton";
+
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<NoDependencies1>();
+            container.Register<NoDependencies2>();
+            container.Register<IService, DependingOn<NoDependencies1, NoDependencies2>>();
+            container.RegisterDecorator<IService, ServiceDecoratorDependingOn<NoDependencies1>>();
+            container.Register<DependingOn<IService>>();
+
+            container.Verify();
+
+            // Act
+            var registration = GetRegistration<DependingOn<IService>>(container);
+
+            // Assert
+            Assert.AreEqual(expectedGraph, GetGraph(registration));
+        }
+
+        [TestMethod]
+        public void Registration_DecoratorWithDecorateeWithOneScopedDependency_ProducesExpectedGraph()
+        {
+            // Arrange
+            string expectedGraph =
+@"DependingOn<IService>( // Async Scoped
+    ServiceDecoratorDependingOn<NoDependencies1>( // Async Scoped
+        DependingOn<NoDependencies1, NoDependencies2>( // Async Scoped
+            NoDependencies1(), // Singleton
+            NoDependencies2()), // Async Scoped
+        NoDependencies1())) // Singleton";
+
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<DependingOn<IService>>();
+            container.RegisterDecorator<IService, ServiceDecoratorDependingOn<NoDependencies1>>();
+            container.Register<IService, DependingOn<NoDependencies1, NoDependencies2>>();
+            container.Register<NoDependencies2>(Lifestyle.Scoped);
+            container.Register<NoDependencies1>();
+
+            container.Verify();
+
+            var registration = GetRegistration<DependingOn<IService>>(container);
+
+            // Assert
+            Assert.AreEqual(expectedGraph, GetGraph(registration));
+        }
+
+        [TestMethod]
+        public void Registration_DecoratorWithSingletonDecorateeAndTransientDependency_ProducesExpectedGraph()
+        {
+            // Arrange
+            string expectedGraph =
+@"DependingOn<IService>( // Transient
+    ServiceDecoratorDependingOn<NoDependencies2>( // Transient
+        DependingOn<NoDependencies1, NoDependencies1>( // Singleton
+            NoDependencies1(), // Singleton
+            NoDependencies1()), // Singleton
+        NoDependencies2())) // Transient";
+
+            Container container = CreateContainerWithAutomicLifestyle();
+
+            container.Register<DependingOn<IService>>();
+            container.Register<IService, DependingOn<NoDependencies1, NoDependencies1>>();
+            container.RegisterDecorator<IService, ServiceDecoratorDependingOn<NoDependencies2>>();
+            container.Register<NoDependencies1>();
+            container.Register<NoDependencies2>(Lifestyle.Transient);
+
+            container.Verify();
+
+            var registration = GetRegistration<DependingOn<IService>>(container);
+
+            // Assert
+            Assert.AreEqual(expectedGraph, GetGraph(registration));
+        }
+
+        private static Container CreateContainerWithAutomicLifestyle()
+        {
+            var container = new Container();
+            container.Options.DefaultLifestyle = Automatic;
+            container.Options.DefaultScopedLifestyle = Scoped;
+            container.Options.ResolveUnregisteredConcreteTypes = false;
+            return container;
+        }
+
+        private static InstanceProducer GetRegistration<T>(Container container) =>
+            container.GetRegistration(typeof(T), throwOnFailure: true);
+
+        private static string GetGraph(InstanceProducer producer) =>
+            producer.VisualizeObjectGraph().Replace($"{nameof(AutomaticLifestyleTests)}.", string.Empty);
+
+        public class DependingOn<T>
+        {
+            public DependingOn(T t)
+            {
+            }
+        }
+
+        public class IService { }
+
+        public class DependingOn<T1, T2> : IService
+        {
+            public DependingOn(T1 t1, T2 t2)
+            {
+            }
+        }
+
+        public class ServiceDecoratorDependingOn<T> : IService
+        {
+            public ServiceDecoratorDependingOn(IService service, T t)
+            {
+
+            }
+        }
+
+        public interface INoDependencies { }
+
+        public class NoDependencies1 : INoDependencies
+        {
+        }
+
+        public class NoDependencies2
+        {
+        }
+    }
+}
---
+++ src/SimpleInjector.CodeSamples/AutomaticLifestyle.cs
@@ -0,0 +1,72 @@
+﻿namespace SimpleInjector.CodeSamples
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public sealed class AutomaticLifestyle : Lifestyle
+    {
+        internal static readonly AutomaticLifestyle Instance = new AutomaticLifestyle();
+
+        public AutomaticLifestyle() : base("Automatic") { }
+
+        public override int Length
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        protected override Registration CreateRegistrationCore<TConcrete>(Container container) =>
+            new AutomaticRegistration<TConcrete>(container);
+
+        protected override Registration CreateRegistrationCore<TService>(
+            Func<TService> instanceCreator, Container container) =>
+            throw new NotSupportedException(
+                $"The {nameof(AutomaticLifestyle)} does not support Func<T> registrations. " +
+                "Please register your Func<T> explicitly using the required lifestyle.");
+
+        private sealed class AutomaticRegistration<TImplementation> : Registration
+            where TImplementation : class
+        {
+            public AutomaticRegistration(Container container) : base(Instance, container)
+            {
+            }
+
+            public override Type ImplementationType => typeof(TImplementation);
+
+            public override Expression BuildExpression()
+            {
+                var expression = this.BuildTransientExpression();
+
+                Lifestyle lifestyle = DetermineLifestyle();
+
+                this.Lifestyle.CreateRegistration<TImplementation>(this.Container).BuildExpression();
+            }
+
+            private Lifestyle DetermineLifestyle()
+            {
+                Lifestyle shortestLifestyle = Singleton;
+
+                foreach (var relationship in this.GetRelationships())
+                {
+                    var dependency = relationship.Dependency;
+
+                    if (shortestLifestyle.Length > dependency.Lifestyle.Length)
+                    {
+                        shortestLifestyle = dependency.Lifestyle;
+                    }
+                }
+
+                return shortestLifestyle;
+            }
+
+            private sealed class DummyRegistration : Registration
+            {
+                public DummyRegistration(Container container) : base(Transient, container)
+                {
+                }
+
+                public override Type ImplementationType => typeof(TImplementation);
+                public override Expression BuildExpression() => this.BuildTransientExpression();
+            }
+        }
+    }
+}
